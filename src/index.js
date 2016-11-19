aa85d5eae4aea12e30f26cf9a2bd30e4320b333f
Geoff Balasi <geoffbalasi@gmail.com>
2016-11-19 15:06:15 -0600

    Working on edit
    newItem is referencing the input. Need to detach it. Something about
    the fact that it is an object.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ var AddItem = React.createClass({
         return (
             <div className="search_container">
             	<h1> Today's Tasks </h1>
-            	<input className="searchbox" type="text" placeholder="Enter Todo List Item" value={this.props.newItem} onChange={this.props.onChange} onKeyPress={this.props.keyPress} />
+            	<input className="searchbox" type="text" placeholder="Enter Todo List Item" value={this.props.newItem.value} onChange={this.props.onChange} onKeyPress={this.props.keyPress} />
                 <input className="button" type="submit" value="Add" onClick={this.props.onAdd} disabled={this.props.disabled} />
             </div>
         );
@@ -17,9 +17,10 @@ var AddItem = React.createClass({
 var ItemList = React.createClass({
 	render: function(){	
 		var remove = this.props.removeItem;
+		var edit = this.props.edit;
 		var todos = this.props.items.map(function(item, index){
-            return <div className="item">
-            			<div key={index} > {item} </div>
+            return <div key={index} className="item">
+            			<Item item={item} edit={edit} />
             			<div className="delete" onClick={remove} id={index}> x </div>
             			<div className="clear"> </div>
             		</div>
@@ -30,17 +31,29 @@ var ItemList = React.createClass({
 	}
 });
 
+var Item = React.createClass({
+	render: function(){
+		if (!this.props.item.editable) {
+			return <div> {this.props.item.value} </div>
+		}
+		else {
+			return <input type="text" defaultValue={this.props.item.value} onChange={this.props.edit} />
+		}
+	}
+});
+
 var App = React.createClass({
     getInitialState: function(){
         return {
             items: [],
-            newItem: '',
+            newItem: { value: '' , editable: false },
             disabled: true
         }
     },
     onAdd: function(){
+    	var newItem = this.state.newItem;
         this.setState(function(state){
-        	items: state.items.push(state.newItem)
+        	items: state.items.push(newItem)
         });
         this.resetInput();
         this.state.disabled = true;
@@ -54,7 +67,11 @@ var App = React.createClass({
     	}
 	},
     resetInput: function(){
-    	this.setState({ newItem: '' })
+    	var newItem = this.state.newItem;
+    	newItem.value = '';
+    	this.setState({ 
+    		newItem: newItem
+    	})
     },
     removeItem: function(event){
     	var id = event.target.id;
@@ -62,14 +79,27 @@ var App = React.createClass({
     		items: state.items.splice(id,1);
     	});
     },
+    editItem: function(event){
+    	var id = event.target.id;
+    	var value = event.target.value;
+    	// replace item value with input value
+    	var items = this.state.items;
+    	items[id] = value;
+    	this.setState({
+    		items: items
+    	});
+    },
     onChange: function(event) {
     	this.disabled(event.target.value);
+    	var value = event.target.value;
+    	var newItem = this.state.newItem;
+    	newItem.value = value;
 		this.setState({
-    		newItem : event.target.value,
+    		newItem: newItem
     	})
     },
     disabled: function(text) {
-    	if ( text === '' ) {
+    	if ( text.value === '' ) {
     		this.setState({
 	    		disabled : true
 	    	})
@@ -84,7 +114,7 @@ var App = React.createClass({
         return (
             <div>
                 <AddItem onAdd={this.onAdd} onChange={this.onChange} newItem={this.state.newItem} keyPress={this._handleKeyPress} disabled={this.state.disabled}/>
-                <ItemList items={this.state.items} removeItem={this.removeItem} />
+                <ItemList items={this.state.items} removeItem={this.removeItem} edit={this.editItem} />
             </div>
         );
     }
